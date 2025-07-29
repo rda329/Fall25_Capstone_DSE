acee386e06a6ac809f7897ec921ded905c391255
rda329 <rubielaqu@gmail.com>
2025-07-29 15:48:58 -0400

    Daily Vehicle Counts QB and SIB
    Data Prep to analysis traffic vol. and type pre/post WIM enforcement.

--- Data/WIN_data.py
+++
@@ -1,9 +0,0 @@
-from pyspark.sql import SparkSession
-
-spark = SparkSession.builder \
-    .appName("SpeedTest") \
-    .master("local[*]") \
-    .getOrCreate()
-
-print("Spark started!")
-spark.stop()
